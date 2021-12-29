57be9c61d5763942bdbe585734c062d103cbabf9
M3htbD301 <81451074+M3htbD301@users.noreply.github.com>
2021-12-28 16:35:22 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1=""
 song2=""
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightwristY=0;
 
 function preload() {
     song1=loadSound("gangPlankGalleon.mp3");
@@ -11,8 +15,32 @@ function setup(){
     canvas.center();
     startVideo=createCapture(VIDEO);
     startVideo.hide();
+    poseNet=ml5.poseNet(startVideo,modelLoaded);//initializing poseNet
+    poseNet.on("pose", gotPoses)
 }
 
 function draw() {
     image(startVideo, 0 ,0, 600, 500)
+}
+
+function modelLoaded() {
+    console.log("poseNet is initialized");
+}
+
+function gotPoses(results) {
+    if (results.length>0) {
+        console.log(results);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        console.log("left wrist x value is "+leftWristX);
+
+        leftWristY=results[0].pose.leftWrist.y;
+     console.log("left wrist y value is "+leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        console.log("right wrist x value is "+rightWristX);
+
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("right wrist y value is "+rightWristY);
+    }
 }
